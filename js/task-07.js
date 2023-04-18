499e8bb2a366e2f99b97c4050e6951cb332a3db7
Roksolyanka <roksolyanakushnir@gmail.com>
2023-04-18 15:54:08 +0300

    task 07 - corrected

--- js/task-07.js
+++ js/task-07.js
@@ -2,9 +2,13 @@
 // і змінює інлайн - стиль span#text, оновлюючи властивість font - size.
 //  В результаті, перетягуючи повзунок, буде змінюватися розмір тексту.
 
-const input = document.querySelector('#font-size-control');
-const text = document.querySelector('#text');
+const input = document.querySelector("#font-size-control");
+const text = document.querySelector("#text");
 
-input.addEventListener("input", () => {
-  text.style.fontSize = `${input.value}px`;
+const sizeCurrent = input.value;
+text.style.fontSize = sizeCurrent + "px";
+
+input.addEventListener("input", (event) => {
+  const size = event.currentTarget.value + "px";
+  text.style.fontSize = size;
 });
